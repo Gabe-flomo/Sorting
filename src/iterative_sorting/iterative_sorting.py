8453933cae04a8e23c634ede049a63113d103d38
gabeflomo <gabeflomo821@gmail.com>
2020-02-17 18:27:44 -0600

    finished todays assignment

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -1,26 +1,41 @@
 # TO-DO: Complete the selection_sort() function below 
+# How it works [4,7,2,9,3,8]
+        # start with the current index
+        # so "for i in range(len(arr)):
+        #       CI = i
+        #       arr[i] == 4"
+        #
+        # then for j in range(len(arr)):
+        #           if arr[CI] > arr[j]:
+        #               arr[CI] = arr[j]
 def selection_sort( arr ):
     # loop through n-1 elements
-    for i in range(0, len(arr) - 1):
-        cur_index = i
-        smallest_index = cur_index
-        # TO-DO: find next smallest element
-        # (hint, can do in 3 loc) 
+    for i in range(0,len(arr)-1):
+        smallest = i 
+        
              
 
-
-
-        # TO-DO: swap
-
-
-
+        # finds the smallest element in the list
+        for j in range(smallest+1,len(arr)):
+            if arr[smallest] > arr[j]:
+                smallest = j
+        
+        # swap 
+        arr[i],arr[smallest] = arr[smallest],arr[i]
+        
+         
 
     return arr
 
-
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort( arr ):
-
+    for i in range(len(arr)):
+        for j in range(len(arr)):
+            if arr[i] < arr[j]:
+                temp = arr[j]
+                arr[j] = arr[i]
+                arr[i] = temp
+            
     return arr
 
 
@@ -28,3 +43,7 @@ def bubble_sort( arr ):
 def count_sort( arr, maximum=-1 ):
 
     return arr
+
+
+
+print(selection_sort([8,2,4,6,9,0]))
--- src/searching/searching.py
+++ src/searching/searching.py
@@ -13,11 +13,34 @@ def binary_search(arr, target):
     return -1 # array empty
     
   low = 0
-  high = len(arr)-1
+  high = len(arr)
+  middle = (low + high) // 2
+  found = False
+  arr.sort()
+  i = 0
+  while i < 10:
+    i += 1
+    val = arr[middle]
+    print(f"Middle: {middle} value: {val} target: {target}")
+     
+    if val == target:
+      found = True
+      return val
+    elif val > target:
+      print("Top half")
+      low = middle + 1
+      high = len(arr[low:])
+      middle = (low + high) // 2
+    elif val < target:
+      
+    
 
-  # TO-DO: add missing code
 
-  return -1 # not found
+
+
+  
+
+   # not found
 
 
 # STRETCH: write a recursive implementation of Binary Search 
@@ -28,3 +51,8 @@ def binary_search_recursive(arr, target, low, high):
   if len(arr) == 0:
     return -1 # array empty
   # TO-DO: add missing if/else statements, recursive calls
+
+x = [2,13,6,8,10,24,20]
+t = 13
+
+print(binary_search(x,t))
